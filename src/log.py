a26a1a465c825f73148bf292a025f3b99ce36944
takamasa <takamasa@dhcp154.ht.sfc.keio.ac.jp>
2017-03-21 13:19:52 +0900

    added web status edit function to log.py

--- src/log.py
+++ src/log.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import logging
+import os
 
 def status_logging():
   log_format = '%(asctime)s- %(name)s - %(levelname)s - %(message)s'    
@@ -8,5 +9,14 @@ def status_logging():
   logging.basicConfig(format=log_format, filename=log_path, level=logging.ERROR) 
   logging.error('HTTP') 
 
+def edit_web_status(status):
+  f = open('../public_html/data.json', 'wb') 
+  if(status == 0):
+    f.write('[0]') 
+  else:
+    f.write('[1]') 
+  f.close() 
+
 if __name__ == '__main__':  
   status_logging() 
+  edit_web_status(0) 
