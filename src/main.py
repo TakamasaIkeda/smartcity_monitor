287811c9300eea495920b729002a5d16061b0772
takamasa <takamasa@takamasa-no-MacBook-Air-2.local>
2017-03-28 09:20:43 +0900

    added send_mail to main function

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from status_check import XmppCheck
 from log import db_push
 import json
 from mail import send_mail
+from edit_web_status import edit_web_status 
 
 JSON_PATH = './../secret_data.json'
 
@@ -18,18 +19,21 @@ def main():
 
   ping_status = ping(json['host']) 
   db_push('ICMP',json['host'], ping_status) 
+  send_mail(json['from_addr'], json['to_addr'], 0) 
   web_flag = ping_status
 
   http_status = http_request(json['http_url'], json['http_uid'], json['http_pw']) 
   db_push('HTTP', json['http_url'], http_status) 
+  send_mail(json['from_addr'], json['to_addr'], 1) 
   web_flag = ping_status
 
   xmpp = XmppCheck(json['jid'], json['sox_pw'])
   xmpp_status = xmpp.xmpp_ping() 
   db_push('SOX', json['jid'], xmpp_status) 
+  send_mail(json['from_addr'], json['to_addr'], 2) 
   web_flag = xmpp_status
 
-
+  edit_web_status(web_flag) 
 
 if __name__ == '__main__':
   main() 
